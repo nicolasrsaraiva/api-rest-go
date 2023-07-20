018e5a552d453aefc7b4ecbd0a37e42a56dee43b
Nícolas Saraiva <nicolasrsaraiva@gmail.com>
2023-07-20 14:05:45 -0300

    feat: added new controllers and middleware

--- routes/routes.go
+++ routes/routes.go
@@ -6,12 +6,17 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/nicolasrsaraiva/api-rest-go/controllers"
+	"github.com/nicolasrsaraiva/api-rest-go/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000", r))
 }
